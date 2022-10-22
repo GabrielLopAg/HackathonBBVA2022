3851e229d2d4950ea266f4c5f6dfc2b4b233bcf3
Gabriel Lopez <gla280900@gmail.com>
2022-10-22 10:52:16 -0500

    add cors

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 description = """
 API para la solución PyME
@@ -12,6 +13,16 @@ app = FastAPI(
     openapi_url="/openapi.json",
     description=description,
     version="0.1.0",
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 from endpoints import api_router
