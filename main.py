6dc38f0f1ce5e0b3833b85a2bae009b7b245898a
Gabriel Lopez <gla280900@gmail.com>
2022-10-22 11:45:08 -0500

    add gunicorn_config file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from core import app
 
 if __name__ == "__main__":
-    import uvicorn
-    import os
+    app.run()
+    # import uvicorn
+    # import os
     
-    start = "main:app"
-    uvicorn.run(
-        start, 
-        host="0.0.0.0", 
-        port=80, 
-        reload=True
-        )
+    # start = "main:app"
+    # uvicorn.run(
+    #     start, 
+    #     # host="0.0.0.0", 
+    #     port=80, 
+    #     reload=True
+    #     )
