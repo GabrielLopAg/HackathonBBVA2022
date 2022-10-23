2cc1aae63ab0ccf04886c9e8b4cd930a94b12e92
aMares1108 <chuisangel8@gmail.com>
2022-10-23 05:12:20 -0500

    Subida de un CSV

--- endpoints/home.py
+++ endpoints/home.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
+import csv
+import codecs
 
 
 router = APIRouter()
@@ -28,3 +30,35 @@ def Home():
     Regresa un JSON con el mensaje de bienvenida.
     """
     return {"message": "Bye World"}
+
+@router.post(
+    path="/upload",
+    status_code=200,
+    tags=["CSV"],
+    summary="Procesa un archivo CSV y da una salida JSON",
+    )
+def upload(file: UploadFile = File(...)):
+    """
+    # Procesamiento de CSV
+    Este endpoint devuelve un JSON con los parámetros agregados.
+
+    Parámetros:
+    - Request body:
+        - File: Archivo .csv
+    - Request query:
+        - Ninguno
+    - Request header:
+        - Ninguno
+        
+    - Request cookie:
+        - Ninguno
+    Regresa un JSON con los datos agregados.
+    """
+    csvReader = csv.DictReader(codecs.iterdecode(file.file, 'utf-8'))
+    data = {}
+    for rows in csvReader:             
+        key = rows['Index']  # Assuming a column named 'Index' to be the primary key
+        data[key] = rows  
+    
+    file.file.close()
+    return data
