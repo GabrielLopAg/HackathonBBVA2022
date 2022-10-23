8c95cb5b7afb62e13f131259383a57eac2e01ff4
Gabriel <gla280900@gmail.com>
2022-10-23 05:31:25 -0500

    refactor

--- endpoints/home.py
+++ endpoints/home.py
@@ -17,18 +17,6 @@ def Home():
     """
     # Hola mundo de la aplicación
     Este endpoint muestra un mensaje.
-
-    Parámetros:
-    - Request body:
-        - Niguno
-    - Request query:
-        - Ninguno
-    - Request header:
-        - Ninguno
-        
-    - Request cookie:
-        - Ninguno
-    Regresa un JSON con el mensaje de bienvenida.
     """
     return {"message": "Bye World"}
 
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from core import app
+from fastapi import FastAPI, Request
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+
+
+description = """
+API para la solución PyME
+
+- Scrapping de paginas web
+- Obtiene modelos de credito para PyMEs
+"""
+
+app = FastAPI(
+    title="PyMEs",
+    openapi_url="/openapi.json",
+    description=description,
+    version="0.1.0",
+)
 
 @app.get(
     path="/",
@@ -25,16 +42,17 @@ def hello_world():
     """
     return {"message": "Hello World"}
 
+origins = ["*"]
 
-if __name__ == "__main__":
-    app.run()
-    # import uvicorn
-    # import os
-    
-    # start = "main:app"
-    # uvicorn.run(
-    #     start, 
-    #     # host="0.0.0.0", 
-    #     port=80, 
-    #     reload=True
-    #     )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from endpoints import api_router
+app.include_router(api_router, prefix="/api/v1")
+handler = Mangum(app)
+
